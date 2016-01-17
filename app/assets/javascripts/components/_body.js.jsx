7bcfac238cc69c35f038d0ada3fe31ca70ea8001
Robert Cornell <robert.cornell78@gmai.com>
2016-01-17 16:31:06 -0700

    delete function working

---
+++ app/assets/javascripts/components/_body.js.jsx
@@ -0,0 +1,42 @@
+var Body = React.createClass({
+  getInitialState() {
+    return { skills: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  handleDelete(id) {
+    $.ajax({
+      url: `/api/v1/skills/${id}`,
+      type: 'DELETE',
+      success: () => {
+        this.removeIdeaFromDOM(id);
+      }
+    });
+  },
+
+  removeIdeaFromDOM(id) {
+    var newSkills = this.state.skills.filter((skill) => {
+      return skill.id != id;
+    });
+
+    this.setState({ skills: newSkills });
+  },
+
+  handleSubmit(skill) {
+    var newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState })
+  },
+
+  render() {
+    return (
+      <div>
+        <NewSkill handleSubmit={this.handleSubmit} />
+        <AllSkills skills={this.state.skills} handleDelete={this.handleDelete} />
+      </div>
+    )
+  }
+});
+
